09b5d47e467d113d31eb3c56435c5b4b8a186dfe
bryson gaebler <bw3m.j.g@gmail.com>
2026-05-21 06:16:31 -0600

    improved menu and menu operations

--- scripts.js
+++ scripts.js
@@ -3,16 +3,50 @@ var editor = new InteractiveEditor(editorDiv);
 
 requestAnimationFrame(() => {
     requestAnimationFrame(() => {
-        editor.value.content = "the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the\nthe the\nthe the the\nthe the\nthe the the the\nthe the\nthe the the\nthe the\nthe the the the the the the \n";
-
-        //
-
-        requestAnimationFrame(() => {
-            requestAnimationFrame(() => {
-                editor.update();
-            });
-        });
+        init();
     });
 });
 
+function init() {
+    editor.value.language = "js";
+    editor.value.content = "";
+
+    editor.update();
+
+    document.querySelectorAll("div.menu").forEach((element) => {
+        element.addEventListener("click", () => {
+            if (!document.body.classList.contains("isMenuOpenable")) {
+                event.stopPropagation();
+                document.body.classList.add("isMenuOpenable");
+            }
+        });
+    });
+
+    document.querySelectorAll("div.menu").forEach((element) => {
+        element.addEventListener("mouseenter", () => {
+            document.querySelectorAll("div.menu").forEach((el) => {
+                el.classList.remove("active");
+            });
+            element.classList.add("active");
+        });
+    });
+
+    document.querySelectorAll("div.action").forEach((element) => {
+        element.addEventListener("click", () => {
+            const action = element.getAttribute("action").toLowerCase();
+
+            switch (action) {
+                case "fopen":
+                    console.log("fopend");
+                    break;
+            }
+        });
+    });
+
+    document.addEventListener("click", () => {
+        if (document.body.classList.contains("isMenuOpenable")) {
+            document.body.classList.remove("isMenuOpenable");
+        }
+    });
+}
 
